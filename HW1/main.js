dca54c7381ad5e976129404b0ccf83501a2c6e3e
nitin31 <nitinsharma0389@gmail.com>
2015-01-22 00:49:42 -0500

    HW1

--- HW1/main.js
+++ HW1/main.js
@@ -2,6 +2,7 @@ var AWS = require('aws-sdk');
 var s = new AWS.S3();
 var s3 = new AWS.S3({params: {Bucket: 'createdbucket', Key: 'myKey'}});
 
+// List the existing buckets.
 s.listBuckets(function(err, data) {
 	if (err) { console.log("Error"); }
 	else {
@@ -18,12 +19,15 @@ s.listBuckets(function(err, data) {
 	}
 });
 
+//Use API method to create a Bucket in S3.
 s3.createBucket(function(err) {
 	if (err) { console.log("Error:", err); }
 	else {
 		console.log("Bucket creation in Progress...");
 	}
 });
+
+//List the available buckets after creating the new bucket in S3.
 setTimeout(function() {
 	s.listBuckets(function(err, data) {
 		if (err) { console.log("Error"); }
